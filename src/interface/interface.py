0d6c4601e863f62d7d136ec3059de3726d9facd9
Raja <yashrjskasana2007@gmail.com>
2025-12-13 16:39:07 +0530

    a lot of work on db has been done today

---
+++ src/interface/interface.py
@@ -0,0 +1,65 @@
+from simple_term_menu import TerminalMenu
+
+class MenuInterface:
+  def __init__(self, db):
+    self.db = db
+    self.primary_menu_options = [
+      "See your fee detals",
+      "Admin options"
+    ]
+    self.admin_menu_options = [
+      "Add new student",
+      "Chnage detalis",
+      "Save Changes"
+    ]
+
+    self.option_func = {
+      "See your fee detals": self.db.display_record,
+      "Admin options": self.admin_menu,
+      "Add new student":self.db.add_record,
+      "Chnage detalis":self.db.change_record,
+      "Save Changes":self.db.save
+    }
+
+  def run(self):
+    primary_menu = TerminalMenu(
+      self.primary_menu_options,
+      status_bar=" Press Q or Esc to quit"
+    )
+
+    quit_flag = False
+    while not quit_flag:
+      index = primary_menu.show()
+
+      if index is None:
+        quit_flag = True
+        self.db.save()
+        break
+
+      option = self.primary_menu_options[index]
+      func = self.option_func[option]
+      func()
+
+  def admin_menu(self):
+    is_auth = self.db.authorisation()
+    if not is_auth:
+      print("Authorisation faild :<")
+      return
+    admin_menu = TerminalMenu(
+      self.admin_menu_options,
+      status_bar=" Press Q or Esc to quit"
+    )
+
+    quit_flag = False
+    while not quit_flag:
+      index = primary_menu.show()
+
+      if index is None:
+        quit_flag = True
+        self.db.save()
+        break
+
+      option = self.primary_menu_options[index]
+      func = self.option_func[option]
+      func()
+
